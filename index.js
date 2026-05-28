34f0f9540feaf673f99926f590949760c513e88f
manhar98 <manharzapdiya9898@gmail.com>
2026-05-28 12:35:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+
+      loop: true,
+
+      centeredSlides: true,
+
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    });
